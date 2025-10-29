ec2d5dd47eac3a9e1f150fef224dcda84653eafd
Ziyad Kazi <ziyad.kazi@gmail.com>
2025-10-29 18:33:08 -0500

    Implemented the starting logic for moving right and left. Added functionality for disableness of arrow left and right. For future, fix the bug pertaining to disability of arrows.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,33 @@ import { FaAngleLeft } from 'react-icons/fa';
 import { FaAngleRight } from 'react-icons/fa';
 function App() {
   const [idx, setIdx] = useState(0)
+  const [isDisabledLeft, setIsDisabledLeft] = useState(true)
+  const [isDisabledRight, setIsDisabledRight] = useState(false)
   console.log(quiz_questions)
   console.log(quiz_questions["questions"][6]["question"])
   console.log(quiz_questions["quizTitle"])
+
+  const moveLeft = () => {
+    if (idx > 0) {
+      setIdx(idx - 1)
+    }
+    if (idx <= 0) {
+      setIsDisabledLeft(true)
+    } else {
+      setIsDisabledLeft(false)
+    }
+  }
+
+  const moveRight = () => {
+    if (idx < quiz_questions["questions"].length-1) {
+      setIdx(idx + 1)
+    }
+    if (idx <= 0) {
+      setIsDisabledLeft(true)
+    } else {
+      setIsDisabledLeft(false)
+    }
+  }
   
 
   return (
@@ -16,9 +40,10 @@ function App() {
         <h1>{quiz_questions["quizTitle"]}</h1>
         <p>Question Number: {idx+1} out of {quiz_questions["questions"].length}</p>
           <div className = "quiz-content">
-            <div className = "previous">
-            <FaAngleLeft className = "icon"/>
-            </div>
+          <button disabled={isDisabledLeft} className="icon-button" onClick = {moveLeft} >
+            <FaAngleLeft className="icon" />
+          </button>
+            
             <div className = "question-info"> 
               <h2>{quiz_questions["questions"][idx]["question"]}</h2>
               <ul className = "options-box">
@@ -30,7 +55,9 @@ function App() {
               </ul>
             </div>
 
-            <FaAngleRight className = "icon"/>
+          <button disabled={isDisabledRight} className="icon-button" onClick = {moveRight}>
+            <FaAngleRight className="icon" />
+          </button>
             
           </div>
       </div>
